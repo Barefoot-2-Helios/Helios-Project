ad90ff2fa4f618e9bb6bb65c06d1ae49dfcb82c2
letiziagranata <148531975+letiziagranata@users.noreply.github.com>
2024-12-12 14:20:12 +0100

    [Fix] fixed time minigame 1

--- Helios - Project/Helios - Project/Liv1View.swift
+++ Helios - Project/Helios - Project/Liv1View.swift
@@ -12,37 +12,39 @@ struct Liv1View: View {
     @State private var showDetail = false
     
     var body: some View {
-        VStack{
-            VStack(alignment: .center) {
-                // Titolo del livello
-                Text("LEVEL 1")
-                    .fontWeight(.bold)
-                    .foregroundColor(.black)
-                    .font(.system(size: 90))
-                
-                // Immagine del diodo
-                Image("Diode3D") // Assicurati che l'immagine si chiami "diodeImage" e sia nel tuo asset catalog
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width:3700, height: 370)
-                    .padding(90)// Dimensioni dell'immagine
-                
-                
-                // Etichetta "DIODE"
-                Text("DIODE")
-                    .fontWeight(.bold)
-                    .foregroundColor(.senape)
-                    .font(.system(size: 80))
-                
+        NavigationStack {
+            VStack{
+                VStack(alignment: .center) {
+                    // Titolo del livello
+                    Text("LEVEL 1")
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                        .font(.system(size: 90))
+                    
+                    // Immagine del diodo
+                    Image("Diode3D") // Assicurati che l'immagine si chiami "diodeImage" e sia nel tuo asset catalog
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width:3700, height: 370)
+                        .padding(90)// Dimensioni dell'immagine
+                    
+                    
+                    // Etichetta "DIODE"
+                    Text("DIODE")
+                        .fontWeight(.bold)
+                        .foregroundColor(.senape)
+                        .font(.system(size: 80))
+                    
+                }
+            }
+            
+            .navigationDestination(isPresented: $showDetail) { MiniGame1()}
+            .onAppear(){
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    self.showDetail.toggle()
+                }
             }
         }
-          
-                .navigationDestination(isPresented: $showDetail) { MiniGame1()}
-                .onAppear(){
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                        self.showDetail.toggle()
-                    }
-                }
             
         }
         
