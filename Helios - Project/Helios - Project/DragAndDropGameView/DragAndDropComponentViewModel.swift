9b3152fadc90efab8d61f97e04d4c344d30184a8
bilegentile <76962962+BileGentile@users.noreply.github.com>
2024-12-12 10:54:43 +0100

    Changed some names for some assets

--- Helios - Project/Helios - Project/DragAndDropGameView/DragAndDropComponentViewModel.swift
+++ Helios - Project/Helios - Project/DragAndDropGameView/DragAndDropComponentViewModel.swift
@@ -11,9 +11,9 @@ import Foundation
 class DragAndDropComponentViewModel {
     var componentsList: [ComponentModel] {
         [
-            ComponentModel(componentName: "diode", componentImage: "diodeReal", componentDestinationImage: "diodeDestination"),
-            ComponentModel(componentName: "resistor", componentImage: "resistor", componentDestinationImage: "resistorDestination"),
-            ComponentModel(componentName: "connector", componentImage: "Connector", componentDestinationImage: "connectorDestination"),
+            ComponentModel(componentName: "diode", componentImage: "diodeReal", componentDestinationImage: "DiodeDestination"),
+            ComponentModel(componentName: "resistor", componentImage: "resistor", componentDestinationImage: "ResistorDestination"),
+            ComponentModel(componentName: "connector", componentImage: "Connector", componentDestinationImage: "ConnectorDestination"),
             ComponentModel(componentName: "blueCapacitor", componentImage: "BlueCapacitor47", componentDestinationImage: "blueCapacitorDestination")
         ]
         
